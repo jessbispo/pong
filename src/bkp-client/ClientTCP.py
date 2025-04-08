47b0122f8d7d846686b6001669cefed7dafeb082
Jessica Bispo <jessicabispo.work@gmail.com>
2025-04-08 18:32:14 -0300

    adding bkp

---
+++ src/bkp-client/ClientTCP.py
@@ -0,0 +1,34 @@
+import socket #importa modulo socket
+
+TCP_IP = '192.168.15.59' # endereço IP do servidor
+TCP_PORTA_SERVER = 32336 # porta disponibilizada pelo servidor dessa máquina
+TAMANHO_BUFFER = 1024
+
+
+# Criação de socket TCP do cliente
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Conecta ao servidor em IP e porta especifica
+cliente.connect((TCP_IP, TCP_PORTA_SERVER))
+
+# envia mensagem para servidor
+MENSAGEM = input("Type your message for server: ")
+cliente.send(MENSAGEM.encode('UTF-8'))
+
+while 1:
+    # recebe dados do servidor
+    dataReceived, addr = cliente.recvfrom(1024)
+    if dataReceived:
+        print ("Received message:", dataReceived)
+        if dataReceived == b"QUIT":
+            cliente.send("QUIT".encode('UTF-8'))
+            print("Chat closed")
+            break
+
+        # envia mensagem para servidor
+        MENSAGEM = input("Type your message for server: ")
+        cliente.send(MENSAGEM.encode('UTF-8'))
+
+# fecha conexão com servidor
+cliente.close()
+
+
